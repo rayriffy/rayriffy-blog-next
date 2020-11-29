352b1d647fad30c4b9e3e0aa15faa9f9c92a8ae9
Phumrapee Limpianchop <git@rayriffy.com>
2020-11-29 14:46:17 +0900

    chore: expose all color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 
 module.exports = {
   purge: [
@@ -8,8 +9,33 @@ module.exports = {
     './src/**/*.jsx',
     './src/**/*.js',
   ],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media',
   theme: {
+    colors: {
+      white: '#ffffff',
+      black: '#000000',
+      bluegray: colors.blueGray,
+      coolgray: colors.coolGray,
+      gray: colors.gray,
+      truegray: colors.trueGray,
+      warmgray: colors.warmGray,
+      red: colors.red,
+      orange: colors.orange,
+      amber: colors.amber,
+      yellow: colors.yellow,
+      lime: colors.lime,
+      green: colors.green,
+      emerald: colors.emerald,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      lightblue: colors.lightBlue,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      violet: colors.violet,
+      purple: colors.purple,
+      pink: colors.pink,
+      rose: colors.rose,
+    },
     extend: {
       typography: {
         DEFAULT: {
