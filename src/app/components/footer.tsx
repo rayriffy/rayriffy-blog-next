942898e0528dd0c0a15e05433f41e7673d07df4c
Phumrapee Limpianchop <git@rayriffy.com>
2020-12-01 01:11:27 +0900

    fix: footer padding

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer: React.FC = React.memo((props) => {
   return (
     <footer className="bg-white">
-      <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto px-4 overflow-hidden sm:px-6 lg:px-8">
         {/* <nav
           className="-mx-5 -my-2 flex flex-wrap justify-center"
           aria-label="Footer"
