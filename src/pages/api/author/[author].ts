9c7d67891072ebec01d2c35e263b7e40a982608d
Phumrapee Limpianchop <git@rayriffy.com>
2020-12-02 03:45:31 +0900

    feat: api

---
+++ src/pages/api/author/[author].ts
@@ -0,0 +1,76 @@
+import { NextApiHandler } from 'next'
+import { omit } from 'lodash'
+
+import { blogPostField } from '../../../core/constants/blogPostField'
+
+import { BlogPost } from '../../../core/@types/BlogPost'
+
+interface RawQueryResult {
+  data: {
+    blogPostCollection: {
+      items: BlogPost[]
+    }
+  }
+}
+
+const api: NextApiHandler = async (req, res) => {
+  const { page, author } = req.query
+
+  try {
+    const query = `
+      query ($author: String) {
+        blogPostCollection(where: { author: { user: $author } }, skip: ${
+          Number(page) * 2
+        }, limit: 6, order: [date_DESC]) {
+          items {
+            ${blogPostField}
+          }
+        }
+      }
+    `
+
+    const queryResult: RawQueryResult = await fetch(
+      `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accepts: 'application/json',
+          Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+        },
+        body: JSON.stringify({
+          query,
+          variables: {
+            author,
+          },
+        }),
+      }
+    ).then(o => o.json())
+
+    if (queryResult.data.blogPostCollection.items.length !== 0) {
+      return res.status(200).send({
+        status: 'success',
+        code: 200,
+        data: queryResult.data.blogPostCollection.items.map(item => ({
+          url: `https://blog.rayriffy.com/${item.slug}`,
+          ...omit(item, ['content', 'categoryCollection', 'author']),
+        })),
+      })
+    } else {
+      return res.status(404).send({
+        status: 'failed',
+        code: 404,
+        data: 'end of page',
+      })
+    }
+  } catch (e) {
+    console.error(e)
+    return res.status(500).send({
+      status: 'failed',
+      code: 500,
+      data: 'crashed',
+    })
+  }
+}
+
+export default api
