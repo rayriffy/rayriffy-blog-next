a092c63f656acbbb737f3179872f0b082dadb2e8
Phumrapee Limpianchop <git@rayriffy.com>
2021-02-17 16:19:39 +0900

    chore: preact

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 const withPlugins = require('next-compose-plugins')
 
+const withPreact = require('next-plugin-preact')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-module.exports = withPlugins([[withBundleAnalyzer]], {
+module.exports = withPlugins([[withPreact], [withBundleAnalyzer]], {
   target: 'serverless',
   async rewrites() {
     return [
