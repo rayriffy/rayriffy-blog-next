fc2e45afa9c83ec52609cf3fc31075d89676e075
Phumrapee Limpianchop <git@rayriffy.com>
2021-02-17 16:23:35 +0900

    Revert "chore: preact"
    This reverts commit a092c63f656acbbb737f3179872f0b082dadb2e8.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 const withPlugins = require('next-compose-plugins')
 
-const withPreact = require('next-plugin-preact')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-module.exports = withPlugins([[withPreact], [withBundleAnalyzer]], {
+module.exports = withPlugins([[withBundleAnalyzer]], {
   target: 'serverless',
   async rewrites() {
     return [
