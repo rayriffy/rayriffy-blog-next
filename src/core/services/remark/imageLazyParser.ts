69561197f27843b9b6f75b457736dae9880d8c91
Phumrapee Limpianchop <git@rayriffy.com>
2021-03-29 13:24:57 +0000

    chore: staged changes

--- src/core/services/remark/imageLazyParser.ts
+++ src/core/services/remark/imageLazyParser.ts
@@ -1,3 +1,5 @@
+import { stringify } from 'querystring'
+
 import { Plugin } from 'unified'
 import { selectAll } from 'unist-util-select'
 
@@ -6,6 +8,16 @@ import { encode } from 'html-entities'
 import { MarkdownNode } from '../../@types/MarkdownNode'
 import { ImageNode } from '../../@types/ImageNode'
 
+const isContentfulImage = (url: string) => url.includes('images.ctfassets.net')
+const getContentfulURL = (url: string, type: 'jpg' | 'png' | 'webp') => {
+  const options = stringify({
+    q: 80,
+    fm: type
+  })
+
+  return `${url}?${options}`
+}
+
 export const imageLazyParser: Plugin = () => {
   const transform = async (markdownAST: MarkdownNode) => {
     // transform
@@ -13,13 +25,26 @@ export const imageLazyParser: Plugin = () => {
 
     await Promise.all(
       nodes.map(async node => {
-        const html = `<img src="${encode(node.url)}" alt="${encode(
+        const baseHtml = `<img src="${encode(getContentfulURL(node.url, 'jpg'))}" alt="${encode(
           node.alt
         )}" loading="lazy" />`
 
         node.type = 'html'
         node.children = undefined
-        node.value = html
+
+        if (isContentfulImage(node.url)) {
+          const html = `
+            <picture>
+              <source type="image/webp" srcset="${encode(getContentfulURL(node.url, 'webp'))}">
+              <source type="image/jpeg" srcset="${encode(getContentfulURL(node.url, 'jpg'))}">
+              ${baseHtml}
+            </picture>
+          `
+
+          node.value = html
+        } else {
+          node.value = baseHtml
+        }
       })
     )
   }
