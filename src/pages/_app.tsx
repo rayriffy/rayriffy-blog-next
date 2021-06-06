260a30100c8e47bffaf7b064fa3bc0a75ec4dff2
Phumrapee Limpianchop <git@rayriffy.com>
2021-06-06 16:31:35 +0000

    chore: staged changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 import { NextPage } from 'next'
 import { AppProps } from 'next/app'
-import { useRouter } from 'next/router'
 
 import { AppLayout } from '../app/components/layout'
 
@@ -11,17 +10,9 @@ import '../styles/tailwind.css'
 const App: NextPage<AppProps> = props => {
   const { Component, pageProps } = props
 
-  const { pathname } = useRouter()
-
   return (
     <AppLayout>
       <Component {...pageProps} />
-      {pathname === '/[slug]' && (
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-        />
-      )}
     </AppLayout>
   )
 }
