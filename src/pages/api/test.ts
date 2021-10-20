c3d2a97d46a47821489b9a31dc6ea2d962f4c8b5
Phumrapee Limpianchop <git@rayriffy.com>
2021-10-21 05:00:03 +0900

    chore: staged changes

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -6,6 +6,8 @@ const api: NextApiHandler = (req, res) => {
   return res.send({
     next: fs.readdirSync(path.join(process.cwd(), '.next')),
     src: fs.readdirSync(path.join(process.cwd(), 'src')),
+    cache: fs.readdirSync(path.join(process.cwd(), '.next', 'cache')),
+    static: fs.readdirSync(path.join(process.cwd(), '.next', 'static')),
   })
 }
 
