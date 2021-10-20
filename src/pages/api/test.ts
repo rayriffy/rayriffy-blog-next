0b3cc3f1daba26e166fc635df91692f0a94b85e8
Phumrapee Limpianchop <git@rayriffy.com>
2021-10-21 04:55:18 +0900

    chore: staged changes

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -3,8 +3,10 @@ import fs from 'fs'
 import path from 'path'
 
 const api: NextApiHandler = (req, res) => {
-  const files = fs.readdirSync(path.resolve(process.cwd()))
-  return res.send(files)
+  return res.send({
+    next: fs.readdirSync(path.join(process.cwd(), '.next')),
+    src: fs.readdirSync(path.join(process.cwd(), 'src')),
+  })
 }
 
 export default api
