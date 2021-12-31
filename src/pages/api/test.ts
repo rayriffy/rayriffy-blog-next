35f16fd745ef36260a792f8e8bed9a6eb44f3ab4
Phumrapee Limpianchop <git@rayriffy.com>
2021-12-31 18:07:36 +0900

    Revert "chore: staged changes"
    This reverts commit c3d2a97d46a47821489b9a31dc6ea2d962f4c8b5.

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -6,8 +6,6 @@ const api: NextApiHandler = (req, res) => {
   return res.send({
     next: fs.readdirSync(path.join(process.cwd(), '.next')),
     src: fs.readdirSync(path.join(process.cwd(), 'src')),
-    cache: fs.readdirSync(path.join(process.cwd(), '.next', 'cache')),
-    static: fs.readdirSync(path.join(process.cwd(), '.next', 'static')),
   })
 }
 
