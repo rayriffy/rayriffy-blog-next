3fa2f399465b3c4a218d9473017d3d7cf92189f4
Phumrapee Limpianchop <git@rayriffy.com>
2021-12-31 18:07:39 +0900

    Revert "chore: staged changes"
    This reverts commit 0b3cc3f1daba26e166fc635df91692f0a94b85e8.

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -3,10 +3,8 @@ import fs from 'fs'
 import path from 'path'
 
 const api: NextApiHandler = (req, res) => {
-  return res.send({
-    next: fs.readdirSync(path.join(process.cwd(), '.next')),
-    src: fs.readdirSync(path.join(process.cwd(), 'src')),
-  })
+  const files = fs.readdirSync(path.resolve(process.cwd()))
+  return res.send(files)
 }
 
 export default api
