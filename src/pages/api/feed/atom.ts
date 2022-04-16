8456d3f30319e18b094ccbb2cc9127668d61b61e
Phumrapee Limpianchop <git@rayriffy.com>
2022-04-16 20:49:52 +0900

    refactor: longer cache time

--- src/pages/api/feed/atom.ts
+++ src/pages/api/feed/atom.ts
@@ -7,7 +7,7 @@ const api: NextApiHandler = async (req, res) => {
 
   res.setHeader(
     'Cache-Control',
-    'public, s-maxage=3600, stale-while-revalidate=86400'
+    'public, s-maxage=3600, stale-while-revalidate=2592000'
   )
 
   return res.status(200).send(feed.atom1())
