c7f96a04c5e9612e05cb760715b54ad9857ac616
Phumrapee Limpianchop <git@rayriffy.com>
2022-09-02 03:23:10 +0900

    fix: preview data generation

--- tools/markdownBuilder.ts
+++ tools/markdownBuilder.ts
@@ -22,7 +22,7 @@ const stringifyArray = (strings: (string | number)[]) => {
   switch (command) {
     case 'build':
       const blogPosts = await getBlogPosts({
-        preview: ['true', '1'].includes(process.env.CI ?? '')
+        preview: !['true', '1'].includes(process.env.CI ?? '')
       })
       await Promise.all(
         blogPosts.map(blogPost => {
