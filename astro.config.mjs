052581a05ffc9ae90d91d2eea37540e48bb4a22c
Phumrapee Limpianchop <git@rayriffy.com>
2022-10-16 14:19:45 +0900

    feat: deps update and reading time

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,7 @@ import prefetch from '@astrojs/prefetch'
 /* Remark plugins */
 import { iframeParser } from './src/modules/remark/iframeParser.mjs'
 import { imageParser } from './src/modules/remark/imageParser.mjs'
+import { readingTime } from './src/modules/remark/readingTime.mjs'
 
 // https://astro.build/config
 export default defineConfig({
@@ -41,7 +42,7 @@ export default defineConfig({
     prefetch(),
   ],
   markdown: {
-    remarkPlugins: [iframeParser, imageParser],
+    remarkPlugins: [iframeParser, imageParser, readingTime],
     rehypePlugins: [],
     extendDefaultPlugins: true,
   },
---
+++ src/modules/remark/readingTime.mjs
@@ -0,0 +1,50 @@
+import getReadingTime from 'reading-time'
+
+/**
+ * 
+ * @param {import('mdast').Root} value 
+ * @param {boolean} includeImageAlt 
+ * @returns
+ */
+const one = (value, includeImageAlt = false) => (
+  (node(value) &&
+    (('value' in value && value.value) ||
+      (includeImageAlt && 'alt' in value && value.alt) ||
+      ('children' in value && all(value.children, includeImageAlt)))) ||
+  (Array.isArray(value) && all(value, includeImageAlt)) ||
+  ''
+)
+
+/**
+ * @param {import('mdast').Root} value 
+ * @param {boolean} includeImageAlt
+ * @returns {string}
+ */
+ const all = (values, includeImageAlt) => {
+  /** @type {Array<string>} */
+  const result = []
+  let index = -1
+
+  while (++index < values.length) {
+    result[index] = one(values[index], includeImageAlt)
+  }
+
+  return result.join('')
+}
+
+/**
+ * 
+ * @param {import('mdast').Root} value 
+ * @returns
+ */
+const node = value => Boolean(value && typeof value === 'object')
+
+/** @type {import('@astrojs/markdown-remark').RemarkPlugin} */
+export const readingTime = () => {
+  return async (markdownAST, { data }) => {
+    const textContent = one(markdownAST)
+
+    const readingTime =  getReadingTime(textContent)
+    data.astro.frontmatter.minutesRead = readingTime.text
+  }
+}
